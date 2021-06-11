5018a22045a87c106f529643359b6c0f79cbda0d
Sabainath Rajendran <sabarinathkrajenderan@gmail.com>
2021-06-11 11:20:50 +0530

    Created tsx files

---
+++ src/screens/Main/Reels/Reels.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Reels = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default Reels;
+
+const styles = StyleSheet.create({});
